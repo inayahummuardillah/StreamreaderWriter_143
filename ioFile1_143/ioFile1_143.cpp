812b3b75e9ccb4c6db51068a91865ae81aa00e46
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-06-19 11:06:09 +0630

    melakukan perulangan setiap baris, menampilkan, dan tutup file setelah selesai

--- ioFile1_143/ioFile1_143.cpp
+++ ioFile1_143/ioFile1_143.cpp
@@ -32,5 +32,17 @@ int main() {
 	infile.open("contoohfile.txt");
 
 	cout << endl << ">= membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini 
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
 	
 }
