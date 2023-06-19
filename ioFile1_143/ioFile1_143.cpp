984644198c1647d249102bbfa878779aa2a41be5
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-06-19 10:19:38 +0630

    string baris membuka file mode menulis dan menunjukan ke dalam sebuah nama file

--- ioFile1_143/ioFile1_143.cpp
+++ ioFile1_143/ioFile1_143.cpp
@@ -10,41 +10,4 @@ int main() {
 	ofstream outfile;
 	//menunjukan ke dalam sebuah nama file
 	outfile.open("contohfile.txt");
-
-	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-	//unlimited loop untuk meneulis
-	while (true) {
-		cout << "- ";
-		//mendapatkan setiap karakter dalam satu baris
-		getline(cin, baris);
-		//loop akan berhenti jika anda masukkan karakter q
-		if (baris == "q") break;
-		//menulis dan memasukkan nilai dari 'baris' ke dalam file
-		outfile << baris << endl;
-	}
-	//selesai dalam menulis sekarang tutup filenya
-	outfile.close();
-
-	//membuka file dalam mode membaca
-	ifstream infile;
-	//menunjukan ke sebuah file
-	infile.open("contoohfile.txt");
-
-	cout << endl << ">= membuka dan membaca file " << endl;
-	//jika file ada maka
-	if (infile.is_open())
-	{
-		//melakukan perulangan setiap baris
-		while (getline(infile, baris))
-		{
-			//dan tampilkan disini 
-			cout << baris << '\n';
-		}
-		//tutup file tersebut setelah selesai
-		infile.close();
-	}
-	//jika tidak ditemukan file maka akan menampilkan ini
-	else cout << "Unable to open file";
-	return 0;
 }
