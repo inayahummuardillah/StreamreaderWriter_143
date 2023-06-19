331dbbdd0a4a8fc329f2db20941c2ef4b62c3fa3
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-06-19 11:16:18 +0630

    melaukan perulangan setiap baris, tutup setelah selesai, dan jika tidak memnumakan file akan menampilkan unable to open file

--- ioFileWithPath_143/ioFileWithPath_143.cpp
+++ ioFileWithPath_143/ioFileWithPath_143.cpp
@@ -30,5 +30,25 @@ int main() {
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 	
-	
+	//membuka file dalam mode membaca 
+	ifstream infile;
+	//menunjukan ke sebuah file
+	infile.open(NamaFile,ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini 
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
